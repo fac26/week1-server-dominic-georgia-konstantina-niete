80b953f7478b3094da80a66f619245565dc6b6d6
Niete <nieteis@gmail.com>
2023-01-17 15:00:23 +0000

    server post

--- src/server.js
+++ src/server.js
@@ -2,4 +2,14 @@ const express = require("express");
 
 const server = express();
 
+const secretList = [];
+
+server.post("/", express.urlencoded({extended:false}),(req,res)=>{
+    const secretMsg = req.body.secret;
+    secretList.push({secretMsg});
+    res.redirect("/");
+})
+
+
 module.exports = server;
+
