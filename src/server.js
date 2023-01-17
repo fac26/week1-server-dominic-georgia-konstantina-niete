f1c614b6352a765cb11df5d5da71ca37b44e03b3
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-01-17 14:48:21 +0000

    added get route

--- src/server.js
+++ src/server.js
@@ -3,3 +3,40 @@ const express = require("express");
 const server = express();
 
 module.exports = server;
+
+const secretsArray = [];
+// const post
+
+server.get("/", (request, response) => {
+  const secrets = secretsArray.map((secret) => {
+    return `<li>${secret} - ANON</li>`;
+  });
+  response.send(`
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css" />
+  <title>Document</title>
+</head>
+<body>
+<h1>Dirty Little Secret</h1>
+
+<form method="POST">
+  <label for="secret">Enter your secrets:</label>
+  <input for="secret" id="secret">
+  <button>Tell me</button>
+</form>
+<ul>
+${secrets}
+</ul>
+</body>
+</html>
+  
+
+  `);
+});
+
+// post
