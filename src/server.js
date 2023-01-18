5685b97b89cd226ff00ea415543cd9da735edc35
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-01-18 11:42:37 +0000

    Updated CSS styling

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const server = express();
 
+const staticHandler = express.static("public");
+
+server.use(staticHandler);
+
 const secretList = [];
 
 server.get("/", (req, res) => {
@@ -16,10 +20,18 @@ server.get("/", (req, res) => {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="style.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Roboto:wght@500&display=swap" rel="stylesheet">
   <title>Document</title>
 </head>
 <body>
-<h1>Dirty Little Secret</h1>
+<div id="logo"></div>
+
+<div id="circle">
+  <h1>Dirty Little Secret</h1>
+  <h2>A place to share your deepest darkest secrets anoymously</h2>
+</div>
 
 <form method="POST">
   <label for="secret">Enter your secrets:</label>
