abbc4eb2b83707871caef2fee259dff85c02fb60
Niete <nieteis@gmail.com>
2023-01-18 11:12:47 +0000

    Test 2 working

--- test/2.test.js
+++ test/2.test.js
@@ -3,13 +3,11 @@ const assert = require("node:assert");
 const server = require("../src/server.js");
 
 test("Missing routes", async () => {
-    const app = server.listen(4000);
-    const response = await fetch ("http://localhost:4000/not-working")
+    const app = server.listen(9876);
+    const response = await fetch ("http://localhost:9876/not-working")
     app.close();
 
     assert.equal(response.status, 404);
     const body = await response.text();
-    assert.match(body, /Not found/);
-
-
+    assert.match(body, /not-working/);
 })
